c84cf1faf17fa4dc90ce0e7162698e6ef4f48af4
rafa7lako <rafa7lako@gmail.com>
2026-04-20 13:52:56 +0200

    reworked thre responsiveness of some sections

--- src/components/SubsiteContent/ServicePageSectionTwo.jsx
+++ src/components/SubsiteContent/ServicePageSectionTwo.jsx
@@ -2,19 +2,17 @@ import { SectionHeader } from '../SectionHeader';
 import './ServicePageSectionTwo.css'
 
 export const ServicePageSectionTwo = ({ title, text, images }) => {
-	return (
-		<section className="sectionTwo">
-			<SectionHeader title={title} theme='light' />
-
-			<div>
-				<p className="subSectionTwoText">{text}</p>
-
-				<div className="flex flexGap2">
-					{images.map((img, i) => (
-						<img key={i} className="subSectionTwoImg" src={img} alt="" />
-					))}
-				</div>
-			</div>
-		</section>
-	);
+    return (
+        <section className="sectionTwo">
+            <SectionHeader title={title} theme='light' />
+            <div>
+                <p className="subSectionTwoText">{text}</p>
+                <div className="subSectionTwoImgGrid">
+                    {images.map((img, i) => (
+                        <img key={i} className="subSectionTwoImg" src={img} alt="" />
+                    ))}
+                </div>
+            </div>
+        </section>
+    );
 };
